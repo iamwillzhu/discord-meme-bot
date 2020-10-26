e78accd10a3eaaf290a9eec07319a5e2d18ba7a3
iamwill <19693587+iamwillzhu@users.noreply.github.com>
2020-10-25 23:28:43 -0400

    initial commit

---
+++ bot.py
@@ -0,0 +1,27 @@
+import os
+import discord
+
+from datetime import datetime
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == "time":
+        response = datetime.now()
+        await message.channel.send(response)
+
+        
+client.run(TOKEN)
